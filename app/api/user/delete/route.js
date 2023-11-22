63f554db6bc36e54e2c802e8b6654f3a0e7d72c9
AditRizkii <adityarizkiramadhan3@gmail.com>
2023-11-22 10:19:04 +0700

    feat : rules implementation

--- app/api/user/delete/route.js
+++ app/api/user/delete/route.js
@@ -1,4 +1,4 @@
-import Result from "@/app/result/[id]/page";
+import Result from "@/app/quiz/[id]/page";
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
